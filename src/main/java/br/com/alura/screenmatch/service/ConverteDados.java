d075e9cd0d0fc2a8ec814c41610c02de85483011
Wasse Lucas <wasselucas.silva@gmail.com>
2023-11-29 10:11:30 -0300

    alterando conversao de dados

--- src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -1,24 +1,17 @@
-
 package br.com.alura.screenmatch.service;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-// A classe ConverteDados implementa a interface IConverteDados.
 public class ConverteDados implements IConverteDados {
-
-    // Cria uma instância do ObjectMapper que será usada para converter JSON em objetos Java e vice-versa.
     private ObjectMapper mapper = new ObjectMapper();
 
-    // Este método é usado para converter uma String JSON em um objeto Java do tipo especificado.
     @Override
-    public <T> T ObterDados(String json, Class<T> classe) {
-        try{
-            // Tenta converter a String JSON em um objeto Java do tipo especificado.
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
             return mapper.readValue(json, classe);
-        }catch (JsonProcessingException e){
-            // Se ocorrer um erro durante o processamento do JSON, uma RuntimeException é lançada.
+        } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
